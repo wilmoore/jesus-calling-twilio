e22d1ef0cf728bc116060b32e012326b9d2fd8d0
Wil Moore III <wil.moore@wilmoore.com>
2022-07-08 18:30:05 -0400

    use cdn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const client = require('twilio')(accountSid, authToken);
 
 client.calls
   .create({
-     url: 'http://demo.twilio.com/docs/voice.xml',
+     url: 'https://raw.githubusercontent.com/wilmoore/jesus-calling-twilio/main/voice.xml',
      to: '+17277109318',
      from: '+19804144556'
    })
