aacdbb69d0635d8436126b0d98e4d52f6391dc0a
Wil Moore III <wil.moore@wilmoore.com>
2022-07-11 08:32:29 -0400

    cleanup

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-require('dotenv').config()
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = require('twilio')(accountSid, authToken);
+const { client } = require('./client');
 
 client.calls
   .create({
-     url: 'https://raw.githubusercontent.com/wilmoore/jesus-calling-twilio/main/voice.xml',
-     to: '+17277109318',
-     from: '+19804144556'
-   })
-  .then(call => console.log(call.sid));
+    twiml: '<Response><Play>https://jesuscalling.nyc3.cdn.digitaloceanspaces.com/jesuscalling-20220611.mp3</Play></Response>',
+    to: '+17277109318',
+    from: process.env.TWILIO_PHONE_NUMBER,
+//  statusCallback: 'https://www.myapp.com/events',
+//  statusCallbackMethod: 'POST',
+  })
+  .then((call) => console.log(call.sid));
